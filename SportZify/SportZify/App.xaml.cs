509108a126c6ad6e8281f53238622397dc3eaf49
Kumar Sanu Lal <lalkumarsanu98@gmail.com>
2021-06-17 13:27:34 +0530

    Xamarin.Forms
    Xamarin.Forms

---
+++ SportZify/SportZify/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportZify
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new HomeShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
